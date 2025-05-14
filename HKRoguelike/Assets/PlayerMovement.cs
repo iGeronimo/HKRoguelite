5c3745ff59865d92e505ace8ee36fc38eed5cb9b
iGeron1mo <mathijsL@hotmail.com>
2025-05-14 09:00:43 +0200

    feat: added basic facing direction based on inputs

--- HKRoguelike/Assets/PlayerMovement.cs
+++ HKRoguelike/Assets/PlayerMovement.cs
@@ -7,37 +7,56 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 5f;
     public float gravity = 2f;
     public Vector2 maxSpeed;
+    public float maxJumpSpeed;
+    public bool facingRight = false;
 
     [Header("Objects")]
     public Transform feet;
 
-    private Vector2 _velocity;
+    public Vector2 _velocity;
     private bool _isGrounded = false;
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = this.transform.localScale;
+    }
 
     // Update is called once per frame
     void Update()
     {
         setGrounded();
-        Movement();
         Jump();
+        Movement();
+        ApplyDirection();
         applyGravity();
         clampVelocity();
         ApplyVelocity();
     }
 
+    void ApplyDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) SetFaceRight(false);
+        if (Input.GetKeyDown(KeyCode.A)) SetFaceRight(false);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) SetFaceRight(true);
+        if (Input.GetKeyDown(KeyCode.D)) SetFaceRight(true);
+    }
+
     void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        _velocity = new Vector2(horizontalInput, 0) * speed;
+        _velocity = new Vector2(horizontalInput * speed, _velocity.y);
     }
 
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Jumping");
             if (_isGrounded)
             {
-                _velocity = new Vector2(_velocity.x, jumpForce);
+                Debug.Log("Applying jumpForce");
+                _velocity += new Vector2(0, jumpForce);
                 _isGrounded = false;
             }
         }
@@ -52,20 +71,18 @@ public class PlayerMovement : MonoBehaviour
     {
         return _velocity;
     }
-    
+
     private void setGrounded()
     {
-        RaycastHit hit;
-        if (Physics.SphereCast(feet.position, 0.1f, feet.forward, out hit, 1))
+        if (!_isGrounded)
         {
-            Debug.Log(hit.collider.tag);
-            if (hit.collider.tag == "Ground")
+            RaycastHit2D[] hit = Physics2D.CircleCastAll(feet.position, 0.1f, feet.forward, 1);
+            for (int i = 0; i < hit.Length; ++i)
             {
-                if (!_isGrounded)
+                if (hit[i].collider.tag == "Ground")
                 {
                     _isGrounded = true;
                     _velocity.y = 0;
-                    Debug.Log("Setting grounded");
                 }
             }
         }
@@ -78,12 +95,28 @@ public class PlayerMovement : MonoBehaviour
             _velocity -= new Vector2(0, gravity);
         }
     }
-    
+
     private void clampVelocity()
     {
         if (_velocity.x < maxSpeed.x * -1) _velocity.x = maxSpeed.x * -1;
         if (_velocity.y < maxSpeed.y * -1) _velocity.y = maxSpeed.y * -1;
         if (_velocity.x > maxSpeed.x) _velocity.x = maxSpeed.x;
-        if (_velocity.y > maxSpeed.y) _velocity.y = maxSpeed.y;
+        if (_velocity.y > maxJumpSpeed) _velocity.y = maxJumpSpeed;
+    }
+
+    private void Dash()
+    {
+        //TODO implement the dashing part
+    }
+
+    /// <summary>
+    /// Positions the player to stand left or right
+    /// </summary>
+    /// <param name="_bool">When true player is facing right, false facing left.</param>
+    private void SetFaceRight(bool _bool)
+    {
+        facingRight = _bool;
+        if (facingRight) this.transform.localScale = new Vector3(originalScale.x * -1, originalScale.y, originalScale.z);
+        else this.transform.localScale = originalScale;
     }
 }
